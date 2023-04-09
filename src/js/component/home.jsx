87c261f34aed68e12f4e5c3a137d45998f664a1d
Sabona Rexwinkel <sabona.a.rexwinkel@gmail.com>
2023-04-09 23:11:13 +0200

    update:

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,71 @@
-import React from "react";
+import React, {useState} from 'react';
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
+	const [todo, setTodo] = useState("");
+	const [list, setList] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>New Task</h1>
+			<Task
+				todo={todo}
+				setTodo={setTodo}
+				list={list}
+				setList={setList}
+			/>
+			<h2>Lista de tareas</h2>
+			<ToDoList list={list} setList={setList} />
+		</div>
+	);
+};
+
+const ToDoList = (props) => {
+	return (
+		<div>
+			<div>
+				{props.list.map(function (valor, id) {
+					return <li key={id}>{valor}</li>;
+				})}
+			</div>
+		</div>
+	);
+};
+
+
+const ToDo = (props) => {
+	const clean = (e) => {
+		props.setTodo("");
+	};
+
+	return (
+		<div>
+			<input
+				type="text"
+				value={props.todo}
+				onChange={(e) => props.setTodo(e.target.value)}
+				placeholder="What to Do"
+			/>
+			<button
+				className="btn btn-success col"
+				onClick={() => {
+					props.setList([...props.list, props.todo]);
+					console.log(props.todo);
+					clean();
+				}}>
+				Save
+			</button>
+			<br />
+			<p>{props.todo}</p>
 		</div>
 	);
 };
 
 export default Home;
+
+
+
+
+
+
